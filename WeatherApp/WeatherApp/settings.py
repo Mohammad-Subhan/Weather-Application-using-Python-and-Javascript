489ffe59fa13b729c011f51f93261f96cdef863d
Mohammad Subhan <157013912+Mohammad-Subhan@users.noreply.github.com>
2024-09-16 19:25:21 +0500

    Update settings.py

--- WeatherApp/WeatherApp/settings.py
+++ WeatherApp/WeatherApp/settings.py
@@ -114,9 +114,10 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
-
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+
+# Directory where static files will be collected
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
