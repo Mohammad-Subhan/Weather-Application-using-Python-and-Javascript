a7e8ecc72c50ec398148021993718e78f16414c3
Suubhan <subhankhalid185@gmail.com>
2024-09-16 23:40:00 +0500

    Undo Deployment Changes

--- WeatherApp/WeatherApp/settings.py
+++ WeatherApp/WeatherApp/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-tmq)9zem9^a36*p^_*59&_a-d06wham$q5lrxpea0kv)o)@el$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app']
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
@@ -115,9 +115,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 STATIC_URL = '/static/'
-
-# Directory where static files will be collected
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
